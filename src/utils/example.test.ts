3702ac83589c7f17b189aea6f4c45c203565594e
m.kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2023-06-13 19:56:27 +0300

    feat(husky): added husky scripts

---
+++ src/utils/example.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Example. Remove after project init!', () => {
+    it('should be true', () => {
+        expect(2 + 2).toBe(4);
+    });
+});
