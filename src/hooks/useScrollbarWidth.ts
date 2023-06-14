e7083b07532685986aa052e7d6689948ac7a1116
Vadym Stebakov <vadim.stebakov.94@gmail.com>
2023-06-14 17:52:36 +0300

    build: adjust linters

--- src/hooks/useScrollbarWidth.ts
+++ src/hooks/useScrollbarWidth.ts
@@ -6,12 +6,10 @@ import { scrollbarWidth } from '@xobotyi/scrollbar-width';
  * @returns The scrollbar width.
  */
 export const useScrollbarWidth = (): number => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const [sbw, setSbw] = useState<number>(scrollbarWidth()!);
 
     useEffect(() => {
         const raf = requestAnimationFrame(() => {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             setSbw(scrollbarWidth()!);
         });
 
