2d7bc78a7315cde07dc042829d42bf47b671edcc
m.kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2023-06-14 09:25:32 +0300

    fix(remove): removed api, store, tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { AxiosError } from 'axios';
 import { useRemoveInitialStyles } from '@/hooks/useRemoveInitialStyles';
 import { useSmoothScroll } from '@/hooks/useSmoothScroll';
-import { BASE_PUBLIC_PATH, IS_DEV, STATUS_CODE_FORBIDDEN, STATUS_CODE_UNAUTHORIZED } from '@/constants';
+import { BASE_PUBLIC_PATH } from '@/constants';
 import Router from '@/pages/router';
 
 const App: React.FC = () => {
     useRemoveInitialStyles();
     useSmoothScroll();
 
-    const queryClient = new QueryClient({
-        defaultOptions: {
-            queries: {
-                retry(failureCount, error) {
-                    const isAxiosError = error instanceof AxiosError;
-                    const isTrying = failureCount < 2;
-
-                    if (!isAxiosError) {
-                        return isTrying;
-                    }
-
-                    const statusCode = error.response?.status;
-
-                    if (statusCode === STATUS_CODE_UNAUTHORIZED || statusCode === STATUS_CODE_FORBIDDEN) {
-                        return false;
-                    }
-
-                    return isTrying;
-                },
-            },
-        },
-    });
-
     return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter basename={BASE_PUBLIC_PATH}>
-                <Router />
-            </BrowserRouter>
-            {IS_DEV && <ReactQueryDevtools />}
-        </QueryClientProvider>
+        <BrowserRouter basename={BASE_PUBLIC_PATH}>
+            <Router />
+        </BrowserRouter>
     );
 };
 
--- src/constants.ts
+++ src/constants.ts
@@ -6,14 +6,6 @@ export const API_URL = import.meta.env.VITE_API_URL;
 export const BASE_PUBLIC_PATH = import.meta.env.VITE_BASE_PUBLIC_PATH;
 
 /**
- * API constants
- */
-export const ERROR_CODE_CANCELED = 'ERR_CANCELED';
-export const STATUS_CODE_UNAUTHORIZED = 401;
-export const STATUS_CODE_FORBIDDEN = 403;
-export const LS_JWT_TOKEN = 'jwt-token';
-
-/**
  * Common constants
  */
 export const DEFAULT_TRANSITION = { type: 'linear', duration: 0.15 };
