50dd0377c94a2e30568cf612f1c0aff876cb8f26
Vadym Stebakov <vadim.stebakov.94@gmail.com>
2023-06-26 11:41:10 +0300

    refactor(`useLocalStorage`): update hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,31 +1,45 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
+import type { SetStateValue } from '@/types';
 
 /**
  * Hook to use localStorage to store and manage value with useState.
  * @param key The key to use in localStorage.
  * @param initialValue The initial value to use.
- * @returns A tuple with the stored value, a function to set the value and a function to get the value.
+ * @returns A tuple with the stored value, a function to set the value.
  */
-export const useLocalStorage = (key: string, initialValue: string | null) => {
-    const [storedValue, setStoredValue] = useState<string | null>(() => {
+export const useLocalStorage = <T>(key: string, initialValue: T): [T, SetStateValue<T>] => {
+    const readValue = useCallback((): T => {
         const item = localStorage.getItem(key);
 
-        return item ? item : initialValue;
-    });
+        try {
+            return item ? (JSON.parse(item) as T) : initialValue;
+        } catch (_error) {
+            return item ? (item as T) : initialValue;
+        }
+    }, [initialValue, key]);
 
-    const getValue = useCallback(() => {
-        const item = localStorage.getItem(key);
+    const [storedValue, setStoredValue] = useState<T>(readValue);
 
-        setStoredValue(item);
-    }, [key]);
+    const setValue: SetStateValue<T> = useCallback(
+        (value) => {
+            const newValue = value instanceof Function ? value(storedValue) : value;
 
-    const setValue = useCallback(
-        (value: string | null) => {
-            setStoredValue(value);
-            typeof value === 'string' ? localStorage.setItem(key, value) : localStorage.removeItem(key);
+            if (newValue || typeof newValue === 'string' || typeof newValue === 'number') {
+                localStorage.setItem(key, typeof newValue === 'object' ? JSON.stringify(newValue) : String(newValue));
+            } else {
+                localStorage.removeItem(key);
+            }
+
+            setStoredValue(newValue);
         },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         [key]
     );
 
-    return [storedValue, setValue, getValue] as const;
+    useEffect(() => {
+        setStoredValue(readValue());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return [storedValue, setValue];
 };
