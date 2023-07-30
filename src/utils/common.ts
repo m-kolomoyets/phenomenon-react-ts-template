7fae1d38b4a3caf12d445fb49ad15d2da0afc364
Vadym Stebakov <vadim.stebakov.94@gmail.com>
2023-07-30 17:11:15 +0300

    feat(utils): add tilwind-merge and clsx

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,7 +1,8 @@
-export const noop = () => {
-    return;
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const cn = (...values: ClassValue[]) => {
+    return twMerge(clsx(values));
 };
 
-export const noopReturnNull = () => {
-    return null;
-};
+export const noop = () => {};
