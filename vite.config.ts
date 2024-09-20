92821f63c610ec414413f5027fcd6d3c0808f0ef
Mykola Kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2024-09-20 17:03:58 +0300

    build(imports-order): added prettier import order automation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, loadEnv } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
