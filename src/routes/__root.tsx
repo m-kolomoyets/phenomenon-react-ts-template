3b12c4cda5b951b87bfa0c7d0614217a66cf3340
Vadym Stebakov <vadim.stebakov.94@gmail.com>
2025-02-02 15:56:07 +0200

    feat(utils): add example of usage

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,17 @@
 import { lazy, Suspense } from 'react';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { noopReturnNull } from '@/utils/common';
 
-const TanStackRouterDevtools =
-    process.env.NODE_ENV === 'production'
-        ? noopReturnNull // Render nothing in production
-        : lazy(async () => {
-              const res = await import('@tanstack/router-devtools');
-              return {
-                  default: res.TanStackRouterDevtools,
-              };
-          });
+const TanStackRouterDevtools = import.meta.env.DEV
+    ? lazy(async () => {
+          const res = await import('@tanstack/router-devtools');
+
+          return {
+              default: res.TanStackRouterDevtools,
+          };
+      })
+    : () => {
+          return null;
+      };
 
 export const Route = createRootRoute({
     component() {
